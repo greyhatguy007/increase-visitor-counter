f7029409503879f858bf75c52639bd8610a597f1
greyhatguy007 <77543865+greyhatguy007@users.noreply.github.com>
2022-01-27 11:00:47 +0530

    Update increase.py

--- increase.py
+++ increase.py
@@ -13,11 +13,12 @@ print()
 user = input("ENTER YOUR GITHUB USERNAME :")
 count = int(input("ENTER NUMBER OF VISITOR COUNTS TO INCREASE : "))
 url = "https://github.com/"+user+"/"+user+"/"
+print("HANG ON, THIS MIGHT TAKE A BIT...")
+print("GO GRAB A COFFEE !")
 
 browser = webdriver.Chrome(options=options)
 
-print("HANG ON, THIS MIGHT TAKE A BIT...")
-print("GO GRAB A COFFEE !")
+
 for _1 in range(0,count):
     browser.get(url)
 browser.close()
