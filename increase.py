66c3dd492e275ad9f2c882eb0170ae7d1de8cb0f
Ritvik <77543865+greyhatguy007@users.noreply.github.com>
2022-02-20 18:47:45 +0530

    update with threading
    Reduced time with threading

--- increase.py
+++ increase.py
@@ -1,11 +1,15 @@
+import time
+start = time.time()
+from math import ceil, floor
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+import os
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
 options.headless = True
 
+print()
 print()
 print("Made with \u2764\uFE0F From Ritvik")
 print()
@@ -13,13 +17,48 @@ print()
 user = input("ENTER YOUR GITHUB USERNAME :")
 count = int(input("ENTER NUMBER OF VISITOR COUNTS TO INCREASE : "))
 url = "https://github.com/"+user+"/"+user+"/"
-print("HANG ON, THIS MIGHT TAKE A BIT...")
-print("GO GRAB A COFFEE !")
 
-browser = webdriver.Chrome(options=options)
+opt = input("DO YOU WISH TO ENABLE THREADING ? (Y/N) : ")
+opt = opt.strip().upper()
+
+if opt=="Y":
+    print()
+    print("NOTE : DON'T GIVE HIGH THREADS, POTATO PC'S WILL FRY")
+    threads = int(input("ENTER NUMBER OF THREADS ( 2-5 RECOMMENDED): "))
+    count = int(count/threads)
+    browsers = []
+
+    print("HANG ON, THIS MIGHT TAKE A BIT...")
+    print("GO GRAB A COFFEE !")
+
+    for _i in range(1,threads+1):
+        temp = "browser"+str(_i)
+        temp = webdriver.Chrome(options=options)
+        #temp = webdriver.Chrome()
+        browsers.append(temp)
+
+    for _i in range(0,count):
+        for browser in browsers:
+            browser.get(url)
+        print(round((_i+1)*100/(count),2), " PERCENT DONE")
+
+    for browser in browsers:
+        browser.close()
+
+elif opt=='N':
+    print("HANG ON, THIS MIGHT TAKE A BIT...")
+    print("GO GRAB A COFFEE !")
+
+    browser = webdriver.Chrome(options=options)
 
 
-for _i in range(0,count):
-    browser.get(url)
-    print((_i+1)*100/(count), " PERCENT DONE")
-browser.close()
+    for _i in range(0,count):
+        browser.get(url)
+        print(round((_i+1)*100/(count),2), " PERCENT DONE")
+    browser.close()
+else:
+    print("INVALID CHOICE")
+end = time.time()
+
+print("PROCESS FINISHED, TIME TAKEN :", end-start)
+print()
